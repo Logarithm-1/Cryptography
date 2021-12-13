8b25b01d3e3fc3a43e215cf088be51d488716ac0
Logan Richards <sdrahcir.log@me.com>
2021-12-13 04:16:08 -0700

    Finished the basic symbol Substitution

--- Sources/Cryptography/Ciphers/Subsitution/Symbol/BasicSymbolSubsitution.swift
+++ Sources/Cryptography/Ciphers/Subsitution/Symbol/BasicSymbolSubsitution.swift
@@ -9,8 +9,10 @@ import Foundation
 
 //MARK: - Can be done with ASCII
 //TODO: Gold Bug Cipher 3‡0†2?3
+//TODO: Inuktitut Language
+//TODO: Tom Tom Code ///\\/
 
-//MARK: - Not Basic Symbol Subsitution (i.e. have to figure this shit out)
+//MARK: - Not So Basic Symbol Subsitution (i.e. have to figure this shit out)
 //TODO: 7-Segment Display
 //TODO: Acéré Cipher
 //TODO: Astronomical Symbol
@@ -22,9 +24,19 @@ import Foundation
 //TODO: Elder Futhark ᚠᚢᚦᚨᚱᚲ
 //TODO: Enochian Alphabet
 //TODO: French Sign Language
+//TODO: Ideograms Cipher (Lines, Circles, Dots)
+//TODO: Knots Notation
+//TODO: Mary Stuart Code
+//TODO: Mayan Numerals
+//TODO: Mirror Digits: Two Different Sets?
+//TODO: Music Sheet Cipher
+//TODO: Music Staff Notation
+//TODO: Navy Signals Code
+//TODO: Pigpen Cipher: Has 8 Vairents
 //TODO: StarTrek_KlingonLanguage: ["'", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "ch", "D", "e", "ng", "gh", "h", "i", "j", "Q", "l", "m", "n", "o", "p", "q", "r", "S", "t", "u", "v", "w", "", "y", "tlh"]
-
-
+//TODO: Tally Marks: Two Types
+//TODO: Younger Futhark ᚠᚢᚦᚬᚱᚴ
+//TODO: Zodiac Killer Cipher
 
 //MARK: - Unsorted
 public final class AlphaAngleSystemBranding : SymbolSubsitution {
@@ -112,27 +124,38 @@ public final class ITCZapfDingbats : SymbolSubsitution {
     }
 }
 
-//TODO: Ideograms Cipher (Lines, Circles, Dots)
-//TODO: Inuktitut Language
-
 public final class IokharicLanguage : SymbolSubsitution {
     public init?() {
         super.init(imagePostName: "iokharicLanguage", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], caseSensitive: false, memorizeCase: false)
     }
 }
 
-//TODO: Knots Notation
-//TODO: Lingua Ignota
-//TODO: Lunar Alphabet (L. Katz)
-//TODO: Mary Stuart Code
-//TODO: Mayan Numerals
-//TODO: Mirror Digits
-//TODO: Music Sheet Cipher
-//TODO: Music Staff Notation
-//TODO: Navy Signals Code
-//TODO: Ogham Alphabet
-//TODO: Pigpen Cipher
-//TODO: Rosicrucian Cipher
+/// - Note: `I` = `J` and `U` = `V`
+public final class LinguaIgnota : SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "linguaIgnota", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], caseSensitive: false, memorizeCase: false)
+    }
+}
+
+public final class LunarAlphabet : SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "lunarAlphabet", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "Ñ"], caseSensitive: false, memorizeCase: false)
+    }
+}
+
+/// - Note: Can go vertical, horizontal
+/// - Note: Can be done in UNICODE (Horizontal only though)
+public final class OghamAlphabet : SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "oghamAlphabet", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "y", "z", "UI", "AE", "EA", "IA", "OI", "UI", "AE", "EA", "IA", "OI"], caseSensitive: false, memorizeCase: false)
+    }
+}
+
+public final class Rosicrucian : SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "rosicrucian", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], caseSensitive: false, memorizeCase: false)
+    }
+}
 
 public final class SamuraiAlphabet : SymbolSubsitution {
     public init?() {
@@ -140,27 +163,65 @@ public final class SamuraiAlphabet : SymbolSubsitution {
     }
 }
 
+public final class SemaphoreTrousers : SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "semaphoreTrousers", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], caseSensitive: false, memorizeCase: false)
+    }
+}
 
-//TODO: Semaphore Trousers Cipher
-//TODO: Sheikah Language
-//TODO: Simlish Language
-//TODO: Standard Galactic Alphabet
-//TODO: Symbol Font
-//TODO: Tally Marks
-//TODO: Templars Cipher
-//TODO: Theban Alphabet
-//TODO: Tic-Tac-Toe Cipher
-//TODO: Tom Tom Code ///\\/
-//TODO: Unown Pokemon Alphabet
-//TODO: Voynich Cipher
-//TODO: Vulcan Language (Star Trek)
-//TODO: Webdings Font
-//TODO: Wingdings 2 Font
-//TODO: Wingdings 3 Font
+/// - Note: Might be able to be done in UNICODE
+public final class SymbolFont : SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "symbolFont", alphabet: ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "T", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~"], caseSensitive: true, memorizeCase: false)
+    }
+}
+
+public final class Templars : SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "templars", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], caseSensitive: false, memorizeCase: false)
+    }
+}
+
+public final class ThebanAlphabet : SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "thebanAlphabet", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "."], caseSensitive: false, memorizeCase: false)
+    }
+}
+
+public final class TicTacToe : SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "ticTacToe", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"], caseSensitive: false, memorizeCase: false)
+    }
+}
+
+public final class Voynich : SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "voynich", alphabet: ["A", "F", "H", "I", "K", "O", "P", "S", "T", "Y", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "x", "y", "z"], caseSensitive: true, memorizeCase: false)
+    }
+}
+
+/// - NOTE: Might Be able to be done in UNICODE
+public final class Microsoft_WebdingsFont : SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "microsoft_webdingsFont", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], caseSensitive: true, memorizeCase: false)
+    }
+}
+
+/// - NOTE: Might Be able to be done in UNICODE
+public final class Microsoft_Wingdings2Font : SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "microsoft_wingdings2Font", alphabet: ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~"], caseSensitive: true, memorizeCase: false)
+    }
+}
+
+/// - NOTE: Can be done in UNICODE
+public final class Microsoft_Wingdings3Font : SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "microsoft_wingdings3Font", alphabet: ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ":", ";", "<", "=", ">", "?", "@", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "[", "\\", "]", "^", "_", "`", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "{", "|", "}", "~"], caseSensitive: true, memorizeCase: false)
+    }
+}
+
 //TODO: Wingdings Font
-//TODO: Younger Futhark ᚠᚢᚦᚬᚱᚴ
-//TODO: Zodiac Killer Cipher
-//TODO: Zodiac Sign
 
 
 //MARK: - From TV Shows/Films/Books
@@ -213,6 +274,25 @@ public final class GravityFalls_BillCipher : SymbolSubsitution {
     }
 }
 
+public final class Pokemon_UnownAlphabet : SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "ticTacToe", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "!", "?"], caseSensitive: false, memorizeCase: false)
+    }
+}
+
+public final class SimCity_Simlish : SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "simCity_simlish", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"], caseSensitive: true, memorizeCase: false)
+    }
+}
+
+/// - Note: Shows up in multiple games
+public final class StandardGalacticAlphabet : SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "standardGalacticAlphabet", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "."], caseSensitive: false, memorizeCase: false)
+    }
+}
+
 public final class Stargate_AncientsAlphabet: SymbolSubsitution {
     public init?() {
         super.init(imagePostName: "starget_ancientsAlphabet", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], caseSensitive: false, memorizeCase: false)
@@ -225,6 +305,12 @@ public final class StarWars_AurebeshAlphabet: SymbolSubsitution {
     }
 }
 
+public final class StarTrek_VulcanLanguage: SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "starTrek_vulcanLanguage", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], caseSensitive: false, memorizeCase: false)
+    }
+}
+
 public final class Zelda_GerudoLanguage : SymbolSubsitution {
     public init?() {
         super.init(imagePostName: "zelda_gerudoLanguage", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], caseSensitive: false, memorizeCase: false)
@@ -233,7 +319,7 @@ public final class Zelda_GerudoLanguage : SymbolSubsitution {
 
 public final class Zelda_GoronLanguage : SymbolSubsitution {
     public init?() {
-        super.init(imagePostName: "zelda_goronLanguage", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "#", "$", "(", ")", ",", "-", ".", ":", ";", "?", "~"], caseSensitive: true, memorizeCase: false)
+        super.init(imagePostName: "zelda_goronLanguage", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "#", "$", "(", ")", ",", "-", ".", ":", ";", "?", "~"], caseSensitive: true, memorizeCase: false)
     }
 }
 
@@ -264,5 +350,11 @@ public final class Zelda_HylianLanguage_TheWindWaker: SymbolSubsitution {
 public final class Zelda_HylianLanguage_TwilightPrincess: SymbolSubsitution {
     public init?() {
         super.init(imagePostName: "zelda_hylianLanguage_twilightPrincess", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"], caseSensitive: false, memorizeCase: false)
+    }
+}
+
+public final class Zelda_SheikahLanguage: SymbolSubsitution {
+    public init?() {
+        super.init(imagePostName: "zelda_sheikahLanguage", alphabet: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "-", ".", "?"], caseSensitive: false, memorizeCase: false)
     }
 }
